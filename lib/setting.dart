6bf8e5d0f3f6e7e4d43e40853936956136c59be1
Shahd1623 <shahdghazaly3@gmail.com>
2025-06-15 21:58:20 +0300

    setting fixing

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,3 +1,342 @@
+// import 'package:flutter/material.dart';
+// import 'package:trollocity/app_styles.dart';
+// import 'package:provider/provider.dart';
+// import 'authentication/create_account_screen.dart';
+// import 'authentication/sign_in_screen.dart'; // استيراد الشاشة الصحيحة
+// import 'provider/user_provider.dart';
+// import 'navBar.dart';
+
+// class SettingScreen extends StatelessWidget {
+//   const SettingScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final userState = context.watch<UserProvider>();
+
+//     return Scaffold(
+//       backgroundColor: AppStyles.primarybackground,
+//       appBar: AppBar(
+//         automaticallyImplyLeading: false,
+//         backgroundColor: AppStyles.primarybackground,
+//       ),
+//       body: Align(
+//         alignment: AlignmentDirectional(0, 0),
+//         child: Column(
+//           mainAxisSize: MainAxisSize.max,
+//           children: [
+//             Container(
+//               width: 140,
+//               child: Stack(
+//                 children: [
+//                   Align(
+//                     alignment: AlignmentDirectional(0, 0),
+//                     child: Padding(
+//                       padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+//                       child: Container(
+//                         width: 100,
+//                         height: 100,
+//                         decoration: BoxDecoration(
+//                           color: Colors.white,
+//                           shape: BoxShape.circle,
+//                         ),
+//                         child: Padding(
+//                           padding: EdgeInsets.all(2),
+//                           child: ClipRRect(
+//                             borderRadius: BorderRadius.circular(50),
+//                             child: Image.asset(
+//                               'assets/person.jpg', 
+//                               width: 100,
+//                               height: 100,
+//                               fit: BoxFit.cover,
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             Padding(
+//               padding: EdgeInsetsDirectional.fromSTEB(0, 16, 0, 12),
+//               child: Text(
+//                 userState.userName, // _userName
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   fontFamily: 'Inter Tight',
+//                   color: AppStyles.textLight,
+//                   fontSize: 24,
+//                   fontWeight: FontWeight.w600,
+//                 ),
+//               ),
+//             ),
+//             Padding(
+//               padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+//               child: Text(
+//                 userState.userEmail, // _userEmail
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   fontFamily: 'Inter Tight',
+//                   color: Color(0xFFCCFFFFFF),
+//                   fontSize: 16,
+//                   fontWeight: FontWeight.w600,
+//                 ),
+//               ),
+//             ),
+//             Expanded(
+//               child: Container(
+//                 width: double.infinity,
+//                 height: 400,
+//                 decoration: BoxDecoration(
+//                   color: AppStyles.backgroundColor,
+//                   boxShadow: [
+//                     BoxShadow(
+//                       blurRadius: 3,
+//                       color: Color(0x33000000),
+//                       offset: Offset(
+//                         0,
+//                         -1,
+//                       ),
+//                     )
+//                   ],
+//                   borderRadius: BorderRadius.only(
+//                     bottomLeft: Radius.circular(0),
+//                     bottomRight: Radius.circular(0),
+//                     topLeft: Radius.circular(16),
+//                     topRight: Radius.circular(16),
+//                   ),
+//                 ),
+//                 child: Align(
+//                   alignment: AlignmentDirectional(0, -1),
+//                   child: SingleChildScrollView(
+//                     child: Column(
+//                       mainAxisSize: MainAxisSize.max,
+//                       mainAxisAlignment: MainAxisAlignment.start,
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       children: [
+//                         Padding(
+//                           padding:
+//                               EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+//                           child: Column(
+//                             mainAxisSize: MainAxisSize.max,
+//                             crossAxisAlignment: CrossAxisAlignment.start,
+//                             children: [
+//                               Padding(
+//                                 padding:
+//                                     EdgeInsetsDirectional.fromSTEB(0, 0, 0, 12),
+//                                 child: Text('Settings',
+//                                     style: TextStyle(
+//                                       fontFamily: 'Inter Tight',
+//                                       fontSize: 30,
+//                                     )),
+//                               ),
+//                               Padding(
+//                                 padding:
+//                                     EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+//                                 child: Row(
+//                                   mainAxisSize: MainAxisSize.max,
+//                                   mainAxisAlignment: MainAxisAlignment.start,
+//                                   children: [
+//                                     Padding(
+//                                       padding: EdgeInsetsDirectional.fromSTEB(
+//                                           0, 8, 16, 8),
+//                                       child: Icon(
+//                                         Icons.language_rounded,
+//                                         color: AppStyles.textDark,
+//                                         size: 30,
+//                                       ),
+//                                     ),
+//                                     Expanded(
+//                                       child: Padding(
+//                                         padding: EdgeInsetsDirectional.fromSTEB(
+//                                             0, 0, 12, 0),
+//                                         child: Text('Language',
+//                                             textAlign: TextAlign.start,
+//                                             style: TextStyle(
+//                                               fontFamily: 'Inter Tight',
+//                                               fontSize: 20,
+//                                             )),
+//                                       ),
+//                                     ),
+//                                     Text('English (eng)',
+//                                         textAlign: TextAlign.center,
+//                                         style: TextStyle(
+//                                           color: AppStyles.primarybackground,
+//                                           fontSize: 16,
+//                                         )),
+//                                   ],
+//                                 ),
+//                               ),
+//                               Padding(
+//                                 padding:
+//                                     EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+//                                 child: Row(
+//                                   mainAxisSize: MainAxisSize.max,
+//                                   mainAxisAlignment: MainAxisAlignment.start,
+//                                   children: [
+//                                     Padding(
+//                                       padding: EdgeInsetsDirectional.fromSTEB(
+//                                           0, 8, 16, 8),
+//                                       child: Icon(
+//                                         Icons.lock_open,
+//                                         color: AppStyles.textDark,
+//                                         size: 30,
+//                                       ),
+//                                     ),
+//                                     Expanded(
+//                                       child: Padding(
+//                                         padding: EdgeInsetsDirectional.fromSTEB(
+//                                             0, 0, 12, 0),
+//                                         child: Text('Privacy & Policy ',
+//                                             textAlign: TextAlign.start,
+//                                             style: TextStyle(
+//                                               color: AppStyles.textDark,
+//                                               fontSize: 20,
+//                                             )),
+//                                       ),
+//                                     ),
+//                                   ],
+//                                 ),
+//                               ),
+//                               Padding(
+//                                 padding:
+//                                     EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+//                                 child: Row(
+//                                   mainAxisSize: MainAxisSize.max,
+//                                   mainAxisAlignment: MainAxisAlignment.start,
+//                                   children: [
+//                                     Padding(
+//                                       padding: EdgeInsetsDirectional.fromSTEB(
+//                                           0, 8, 16, 8),
+//                                       child: Icon(
+//                                         Icons.help_outline_rounded,
+//                                         color: AppStyles.textDark,
+//                                         size: 30,
+//                                       ),
+//                                     ),
+//                                     Expanded(
+//                                       child: Padding(
+//                                         padding: EdgeInsetsDirectional.fromSTEB(
+//                                             0, 0, 12, 0),
+//                                         child: Text('Help',
+//                                             textAlign: TextAlign.start,
+//                                             style: TextStyle(
+//                                               color: AppStyles.textDark,
+//                                               fontSize: 20,
+//                                             )),
+//                                       ),
+//                                     ),
+//                                   ],
+//                                 ),
+//                               ),
+//                               Padding(
+//                                 padding:
+//                                     EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+//                                 child: Row(
+//                                   mainAxisSize: MainAxisSize.max,
+//                                   mainAxisAlignment: MainAxisAlignment.start,
+//                                   children: [
+//                                     Padding(
+//                                       padding: EdgeInsetsDirectional.fromSTEB(
+//                                           0, 8, 16, 8),
+//                                       child: Icon(
+//                                         Icons.edit,
+//                                         color: AppStyles.textDark,
+//                                         size: 30,
+//                                       ),
+//                                     ),
+//                                     Expanded(
+//                                       child: Padding(
+//                                         padding: EdgeInsetsDirectional.fromSTEB(
+//                                             0, 0, 12, 0),
+//                                         child: Text('Profile Settings',
+//                                             textAlign: TextAlign.start,
+//                                             style: TextStyle(
+//                                               color: AppStyles.textDark,
+//                                               fontSize: 20,
+//                                             )),
+//                                       ),
+//                                     ),
+//                                     Text('Edit Profile',
+//                                         textAlign: TextAlign.center,
+//                                         style: TextStyle(
+//                                           color: AppStyles.primarybackground,
+//                                           fontSize: 16,
+//                                         )),
+//                                   ],
+//                                 ),
+//                               ),
+//                               Padding(
+//                                 padding:
+//                                     EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+//                                 child: Row(
+//                                   mainAxisSize: MainAxisSize.max,
+//                                   mainAxisAlignment: MainAxisAlignment.start,
+//                                   children: [
+//                                     Padding(
+//                                       padding: EdgeInsetsDirectional.fromSTEB(
+//                                           0, 8, 16, 8),
+//                                       child: Icon(
+//                                         Icons.login_rounded,
+//                                         color: AppStyles.textDark,
+//                                         size: 30,
+//                                       ),
+//                                     ),
+//                                     Expanded(
+//                                       child: Padding(
+//                                         padding: EdgeInsetsDirectional.fromSTEB(
+//                                             0, 0, 12, 0),
+//                                         child: Text('Log out of account',
+//                                             textAlign: TextAlign.start,
+//                                             style: TextStyle(
+//                                               color: AppStyles.textDark,
+//                                               fontSize: 20,
+//                                             )),
+//                                       ),
+//                                     ),
+//                                     GestureDetector(
+//                                       onTap: () {
+//                                         Navigator.pushReplacement(
+//                                           context,
+//                                           MaterialPageRoute(
+//                                               builder: (context) =>
+//                                                   SignInScreen()), // استبدال AuthScreen بـ SignInScreen
+//                                         );
+//                                       },
+//                                       child: MouseRegion(
+//                                         cursor: SystemMouseCursors.click,
+//                                         child: Text(
+//                                           'Log Out?',
+//                                           textAlign: TextAlign.center,
+//                                           style: TextStyle(
+//                                             color: const Color.fromARGB(
+//                                                 255, 221, 11, 11),
+//                                             fontSize: 16,
+//                                             decorationColor: Color.fromARGB(
+//                                                 255, 221, 11, 11),
+//                                           ),
+//                                         ),
+//                                       ),
+//                                     )
+//                                   ],
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       bottomNavigationBar: const ButtomNavbar(currentIndex: 4),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:trollocity/app_styles.dart';
 import 'package:provider/provider.dart';
@@ -44,7 +383,7 @@ class SettingScreen extends StatelessWidget {
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(50),
                             child: Image.asset(
-                              'assets/person.jpg', 
+                              'assets/person.jpg', // غيّر المسار حسب اسم الملف اللي عندك
                               width: 100,
                               height: 100,
                               fit: BoxFit.cover,
